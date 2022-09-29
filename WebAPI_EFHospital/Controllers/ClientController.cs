6b47d0a09972cfa3bede67de7173b93080c93074
Regber <regber.888@gmail.com>
2022-09-29 16:23:03 +0300

    Добавлен контроллер Client

---
+++ WebAPI_EFHospital/Controllers/ClientController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI_EFHospital.Controllers
+{
+    public class ClientController
+    {
+    }
+}
